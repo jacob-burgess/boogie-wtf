c0b17d455b69b50d2c2b8b05c0f150db33fde29e
Jacob Burgess <jacobburgess11@gmail.com>
2023-11-19 18:03:08 -0800

    revert commented code

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,7 +1,7 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { type NextRequest } from "next/server";
 
-// import { env } from "~/env.mjs";
+import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
@@ -12,13 +12,13 @@ const handler = (req: NextRequest) =>
     router: appRouter,
     createContext: () => createTRPCContext({ req }),
     onError:
-      // env.NODE_ENV === "development" ?
-      ({ path, error }) => {
-        console.error(
-          `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-        );
-      },
-    // : undefined,
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+            );
+          }
+        : undefined,
   });
 
 export { handler as GET, handler as POST };
