b2677ae43f02e535bad3df80d8fd3a3f167739c1
Jacob Burgess <jacobburgess11@gmail.com>
2023-11-19 19:03:53 -0800

    init shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,68 +1,76 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
-
-export default {
-  content: ["./src/**/*.tsx"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
+    },
+    extend: {
       colors: {
-        theme: {
-          light: "#d6e2f8",
-          dark: "#010732",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        boogie: {
-          50: "#b3c5e6",
-          100: "#93abd5",
-          200: "#5e7cb3",
-          300: "#375893",
-          400: "#1d3c78",
-          500: "#0e2a61",
-          600: "#061e50",
-          700: "#031844",
-          800: "#01133c",
-          900: "#011136",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        "boogie-turqoise": {
-          50: "#b6ecef",
-          100: "#99e3e7",
-          200: "#65d3d5",
-          300: "#3bc2c3",
-          400: "#1cb0b0",
-          500: "#089d9d",
-          600: "#008789",
-          700: "#007074",
-          800: "#00585e",
-          900: "#004148",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        "boogie-purple": {
-          50: "#d2b5ef",
-          100: "#c096e5",
-          200: "#a160d2",
-          300: "#8936bf",
-          400: "#7417ac",
-          500: "#630498",
-          600: "#550084",
-          700: "#470070",
-          800: "#38005b",
-          900: "#2a0047",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
-        "boogie-green": {
-          50: "#cef0bd",
-          100: "#b4e9a5",
-          200: "#7dd979",
-          300: "#54c861",
-          400: "#37b750",
-          500: "#21a440",
-          600: "#138f2e",
-          700: "#0a7919",
-          800: "#056206",
-          900: "#0e4a02",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")],
+}
