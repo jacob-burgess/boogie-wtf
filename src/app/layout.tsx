e3b59f71025a57e74e44756231d50d6e97805637
Jacob Burgess <jacobburgess11@gmail.com>
2023-11-19 22:24:54 -0800

    jk one more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,21 +40,32 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     locale: "en_US",
-    siteName: siteConfig.name,
+    url: siteConfig.url,
     title: siteConfig.name,
     description: siteConfig.description,
-    images: {
-      url: siteConfig.ogImage,
-      width: 1200,
-      height: 630,
-      alt: siteConfig.name,
-    },
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@jacoboogie",
   },
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
