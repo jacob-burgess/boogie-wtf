263ce4fce14f2cf5a033c9712d4db9a84e62e8de
Jacob Burgess <jacobburgess11@gmail.com>
2023-11-25 17:42:12 -0800

    getting error due to "hls.js" package... fix incoming here https://github.com/vercel/next.js/pull/58572

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import { withNextVideo } from "next-video/process";
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -7,4 +8,4 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+export default withNextVideo(config);
--- src/env.mjs
+++ src/env.mjs
@@ -31,6 +31,8 @@ export const env = createEnv({
     // Add ` on ID and SECRET if you want to make sure they're not empty
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string(),
+    MUX_TOKEN_ID: z.string(),
+    MUX_TOKEN_SECRET: z.string(),
   },
 
   /**
@@ -53,6 +55,8 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    MUX_TOKEN_ID: process.env.MUX_TOKEN_ID,
+    MUX_TOKEN_SECRET: process.env.MUX_TOKEN_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
