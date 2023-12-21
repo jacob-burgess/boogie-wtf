4484927074fbf0642650378e62a7d4e1b1235b52
Jacob Burgess <jacobburgess11@gmail.com>
2023-12-20 21:17:05 -0800

    comment out pics

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,5 +8,5 @@ export default {
   dbCredentials: {
     connectionString: env.DATABASE_URL,
   },
-  tablesFilter: ["boogie-wtf_*"],
+  tablesFilter: ["wtf_*"],
 } satisfies Config;
--- src/app/life/page.tsx
+++ src/app/life/page.tsx
@@ -1,10 +1,9 @@
-import ImageCardDisplay from "~/components/image-card-display";
-
 export default function Home() {
   return (
     <main className="container flex flex-col items-center justify-center p-4">
-      <p className="mb-8 text-4xl font-extrabold">Some photos i like</p>
-      <ImageCardDisplay />
+      <p>hi</p>
+      {/* <p className="mb-8 text-4xl font-extrabold">Some photos i like</p>
+      <ImageCardDisplay /> */}
     </main>
   );
 }
