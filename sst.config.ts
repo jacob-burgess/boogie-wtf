533f597ffca21e1de6e68dec5515d97feb20522d
Jacob Burgess <jacobburgess11@gmail.com>
2025-02-19 10:58:07 -0800

    sst setup

--- sst.config.ts
+++ sst.config.ts
@@ -1,4 +1,5 @@
 /// <reference path="./.sst/platform/config.d.ts" />
+import { readdirSync } from "fs";
 
 export default $config({
   app(input) {
@@ -6,7 +7,22 @@ export default $config({
       name: "boogie-wtf",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: {
+        aws: {
+          region: "us-east-1",
+          profile:
+            input?.stage === "production" ? "boogie-production" : "boogie-dev",
+        },
+        cloudflare: true,
+      },
     };
   },
-  async run() {},
+  async run() {
+    const outputs = {};
+    for (const value of readdirSync("./infra/")) {
+      const result = await import(`./infra/${value}`);
+      if (result.outputs) Object.assign(outputs, result.outputs);
+    }
+    return outputs;
+  },
 });
