eead27ef277a4642bd86e186d563c7db163a2969
Jacob Burgess <jacobburgess11@gmail.com>
2025-02-19 10:53:24 -0800

    restart

--- src/app/life/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="container flex flex-col items-center justify-center p-4">
-      <p>hi</p>
-      {/* <p className="mb-8 text-4xl font-extrabold">Some photos i like</p>
-      <ImageCardDisplay /> */}
-    </main>
-  );
-}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import AboutMe from "~/components/about-me";
-
-export default function Home() {
-  return (
-    <main className="container flex flex-col items-center justify-center p-4">
-      <AboutMe />
-    </main>
-  );
-}
